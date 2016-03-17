b5943092a3cdf65cd31c08afbf97cf800fba1fc7
Scamwell <smscannell@gmail.com>
2016-03-17 15:13:23 +0000

    Added PID
    - Added PID control system
    - Added smoothing for the yaw measurements
    - Changed variable names to align with spec
    - Added Arduino pin interfaces

---
+++ MPU-9150/MPU-9150_PID/MPU-9150_PID.ino
@@ -0,0 +1,148 @@
+// USER DEFINABLE VARIABLES
+
+//Pin assignments
+#define ClockwiseWinch 2
+#define AntiClockwiseWinch 3
+#define V_sig 5   // Must be pwm pin
+
+//PID parameters
+double Kp = 1, Ki = 2, Kd = 0, Setpoint = 0;
+
+// http://www.instructables.com/id/Simple-Manual-Magnetometer-Calibration/?ALLSTEPS
+// Magetometer offsets:
+#define MPU9150addr 0x68
+#define MXOFFSET -53
+#define MYOFFSET 68
+#define MZOFFSET 58
+
+#include "Wire.h"
+
+// I2Cdev and MPU6050 must be installed as libraries, or else the .cpp/.h files
+// for both classes must be in the include path of your project
+#include <I2Cdev.h>
+#include "MPU6050.h"
+#include "math.h"
+#include <PID_v1.h>
+
+// class default I2C address is 0x68
+// specific I2C addresses may be passed as a parameter here
+// AD0 low = 0x68 (default for InvenSense evaluation board)
+// AD0 high = 0x69
+MPU6050 accelgyro(MPU9150addr);
+
+float V_meas, fax, fay, faz, fgx, fgy, fgz, fmx, fmy, fmz, Anti_status, Clock_status, smoothedV_meas;
+boolean AccelWkg; // Accelerometer present and working?
+
+// Setup PID
+double Input, Output; //  Define variables
+PID myPID(&Input, &Output, &Setpoint, Kp, Ki, Kd, DIRECT); // Specify the links and initial tuning parameters
+
+
+void setup() {
+  // put your setup code here, to run once:
+  Wire.begin();
+  //------------------------ PID Setup ----------------------------------------//
+  pinMode(ClockwiseWinch, OUTPUT);
+  pinMode(AntiClockwiseWinch, OUTPUT);
+  pinMode(V_sig, OUTPUT);
+
+  Setpoint = 0;    // Desired orientation in degrees
+  myPID.SetOutputLimits(-255, 255); //  Tell the PID what range of outputs to give
+  myPID.SetMode(AUTOMATIC); // Turn PID on
+
+  //------------------------ IMU Setup ---------------------------------------//
+
+  // initialize serial communication
+  // (38400 chosen because it works as well at 8MHz as it does at 16MHz, but
+  // it's really up to you depending on your project)
+  Serial.begin(38400);
+
+  // initialize device
+  Serial.println("Initializing I2C devices...");
+  accelgyro.initialize();
+
+  // verify connection
+  Serial.println("Testing device connections...");
+  AccelWkg = accelgyro.testConnection();
+  Serial.println(AccelWkg ? "MPU6050 connection successful" : "MPU6050 connection failed");
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  char line[256];
+
+  if (AccelWkg) {
+    readAccel();
+    readMag();
+    // smoothing
+    smoothedV_meas += (V_meas - smoothedV_meas) / 10;
+
+    // PID
+    if (abs(smoothedV_meas - Setpoint) > 2) {
+      Input = smoothedV_meas;
+      myPID.Compute();
+      analogWrite(V_sig, abs(Output));
+    }
+
+    // Relays
+    if (Output > 0) {
+      digitalWrite(ClockwiseWinch, HIGH);
+      digitalWrite(AntiClockwiseWinch, LOW);
+      Anti_status = 0;
+      Clock_status = 1;
+    }
+    else if (Output < 0) {
+      digitalWrite(ClockwiseWinch, LOW);
+      digitalWrite(AntiClockwiseWinch, HIGH);
+      Anti_status = 1;
+      Clock_status = 0;
+    }
+    else  {
+      digitalWrite(ClockwiseWinch, LOW);
+      digitalWrite(AntiClockwiseWinch, LOW);
+      Anti_status = 0;
+      Clock_status = 0;
+    }
+
+
+
+
+    // display tab-separated accel/gyro x/y/z values
+    Serial.print(V_meas); Serial.print("\t");
+    Serial.print(smoothedV_meas); Serial.print("\t");
+    Serial.print(abs(Output)); Serial.print("\t");
+    Serial.print(Anti_status); Serial.print("\t");
+    Serial.println(Clock_status);
+    delay(10);
+  }
+}
+
+void readAccel() {
+  int16_t ax, ay, az;
+  int16_t gx, gy, gz;
+  char line[256];
+  // read raw accel/gyro measurements from device
+  accelgyro.getMotion6(&ax, &ay, &az, &gx, &gy, &gz);
+  // these methods (and a few others) are also available
+  //accelgyro.getAcceleration(&ax, &ay, &az);
+  //accelgyro.getRotation(&gx, &gy, &gz);
+  // Convert accel to G
+  fax = float(ax) / 16384; fay = float(ay) / 16384; faz = float(az) / 16384;
+
+  // Convert gyro to float deg/sec
+  fgx = float(gx) / 131.072; fgy = float(gy) / 131.072; fgz = float(gz) / 131.072;
+
+}
+
+void readMag() {
+  int16_t mx, my, mz;
+  accelgyro.getMag(&mx, &my, &mz);
+  // Apply offets to magnetometer
+  mx -= MXOFFSET; my -= MYOFFSET; mz -= MZOFFSET;
+  fmx = float(mx); fmy = float(my); fmz = float(mz);
+
+  V_meas = atan2(-fmy, fmx) * 180 / 3.14159;
+  // if (V_meas < 0) V_meas += 360;
+}
+
